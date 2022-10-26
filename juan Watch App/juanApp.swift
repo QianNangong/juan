2d98ff3f3e20bf189ebbd1951e414a950aaa4458
Keqing Li <me@kaze.ai>
2022-10-26 14:57:00 +0800

    Initial Commit

---
+++ juan Watch App/juanApp.swift
@@ -0,0 +1,17 @@
+//
+//  juanApp.swift
+//  juan Watch App
+//
+//  Created by 刻晴 on 2022/10/26.
+//
+
+import SwiftUI
+
+@main
+struct juan_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
